3566e82c3dfc4a81bc01424ab246f4223c5750c0
Gilead Odo <odogilead@gmail.com>
2023-11-04 13:01:12 +0100

    Cleanup

--- src/layout/appLayout.tsx
+++ src/layout/appLayout.tsx
@@ -3,7 +3,6 @@ import { ComponentType, Fragment } from "react";
 import HeadMeta from "@/components/Head";
 import { AppShell } from "@mantine/core";
 import { Header } from "@/components/NavBar";
-import Footer from "@/components/Footer";
 
 export default function withLayout(
   Component: ComponentType,
@@ -14,16 +13,10 @@ export default function withLayout(
     return (
       <Fragment>
         <HeadMeta pageName={pageName} />
-        <AppShell
-          header={{ height: 60 }}
-          // footer={{ height: 230 }}
-          padding="md"
-        >
+        <AppShell header={{ height: 60 }} padding="md">
           <AppShell.Header>
             <Header />
           </AppShell.Header>
-
-          {/* <AppShell.Footer bg="#0b935c"></AppShell.Footer> */}
 
           <AppShell.Main>
             <Component />
